e735314d4d91de9e6606c600de702afe2adc66ba
zeeshan-ahmad-dev <za5743759@gmail.com>
2024-10-17 07:39:43 +0500

    success alert

--- app.js
+++ app.js
@@ -12,18 +12,25 @@ Points: <span class="count">00</span>
 
 const players =  document.querySelectorAll('.player');
 players.forEach(player => {
-    player.querySelector('.name').textContent = prompt("Enter name of player");
+    playerName = player.querySelector('.name');
+    playerName.textContent = prompt("Enter name of player");
 
     const countSpan = player.querySelector('.count');
     const plusButton = player.querySelector('.plus');
     const minusButton = player.querySelector('.minus');
+    const winner = document.querySelector('.success');
 
     plusButton.addEventListener('click',() => {
         let count = parseInt(countSpan.textContent);
-        countSpan.textContent = count + 1;
-    })
+        if (count + 1 >= 11) {
+            winner.querySelector('.winner').textContent = playerName.textContent;
+            winner.classList.remove('hidden');
+        } else {
+            countSpan.textContent = count + 1;
+        }
+    });
     minusButton.addEventListener('click',() => {
         let count = parseInt(countSpan.textContent);
         countSpan.textContent = count - 1;
-    })
+    });
 })
